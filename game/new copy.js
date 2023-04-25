6ae78e08b6559ece58c05a4ed52a30baf22a045b
emart-1-gbg <enes.mart.gbg22@amerikanskagymnasiet.se>
2023-04-25 14:15:15 +0200

    started on overlay

--- game/new copy.js
+++ game/new copy.js
@@ -2,12 +2,21 @@
 
 const canvas = document.getElementById("mainCanvas")
 const ctx = canvas.getContext("2d")
-const CANVAS_WIDTH = canvas.width = 1920 
-const CANVAS_HEIGHT = canvas.height = 1080
+
+const widthMultiplier = 0.98
+const heightMultiplier = 0.85
+canvas.width = window.innerWidth * widthMultiplier
+canvas.height = window.innerHeight * heightMultiplier
+
+window.addEventListener("resize", (e) => {
+    canvas.width = window.innerWidth * widthMultiplier
+    canvas.height = window.innerHeight * heightMultiplier
+})
 
 const menu = document.getElementById("menus")
+const overlay = document.getElementById("overlay")
 
-let numberOfEnemies = 10
+let numberOfEnemies = 100
 
 let playerCount = 1
 
@@ -39,11 +48,11 @@ const levels = [
         spawn2: {x: 200, y: 550},
     
         topBorder: {x: 100, y: 100, len: 1500}, 
-        leftBorder: {x: 100, y: 100, len: CANVAS_HEIGHT - 200}, 
-        bottomBorder: {x: 100, y: CANVAS_HEIGHT - 130, len: 1500}, 
+        leftBorder: {x: 100, y: 100, len: canvas.height - 200}, 
+        bottomBorder: {x: 100, y: canvas.height - 130, len: 1500}, 
 
         doorwayRight: {top: {x: 1570, y: 100, len: 300}, 
-                        bottom: {x: 1570, y: CANVAS_HEIGHT - 400, len: 300},
+                        bottom: {x: 1570, y: canvas.height - 400, len: 300},
                         
                         topHall: {x: 1570, y: 400, len: 500},
                         bottomHall: {x: 1570, y: 650, len: 500}
@@ -140,9 +149,6 @@ class Controls{
     }
 }
 
-const playerWidth = 40
-const playerHeight = 55
-
 class Player {
 
     static ID = 1
@@ -154,8 +160,8 @@ class Player {
         Player.instance.push(this)
 
 
-        this.w = playerWidth
-        this.h = playerHeight
+        this.w = 30
+        this.h = 40
 
         this.x = null
         this.y = null
@@ -171,7 +177,7 @@ class Player {
         this.maxSpeed = 9
         this.friction = 0.5
 
-        this.isGrounded = false
+        this.health = 30
     }
 
     update(){
@@ -180,8 +186,8 @@ class Player {
 
         // if (this.x < 0 ||
         //     this.y < 0 ||
-        //     this.x + this.w > CANVAS_WIDTH ||
-        //     this.y + this.h > CANVAS_HEIGHT) {
+        //     this.x + this.w > canvas.width ||
+        //     this.y + this.h > canvas.height) {
         //     // console.log("player " + this.id + " is outside")
         // } else {
         //     // console.log("player " + this.id + " is inside")
@@ -245,8 +251,8 @@ class Player {
             this.xSpeed = 0
         }
 
-        if (this.x + this.w >= CANVAS_WIDTH) {
-            this.x = CANVAS_WIDTH - this.w
+        if (this.x + this.w >= canvas.width) {
+            this.x = canvas.width - this.w
             this.xSpeed = 0
         }
 
@@ -255,8 +261,8 @@ class Player {
             this.ySpeed = 0
         }
 
-        if (this.y + this.h > CANVAS_HEIGHT) {
-            this.y = CANVAS_HEIGHT - this.h
+        if (this.y + this.h > canvas.height) {
+            this.y = canvas.height - this.h
             this.ySpeed = 0
         }
 
@@ -277,16 +283,21 @@ class Player {
 
 class Enemy {
     constructor(){
-        this.x = Math.floor(Math.random() * CANVAS_WIDTH + 1)
-        this.y = Math.floor(Math.random() * CANVAS_HEIGHT + 1)
+        this.x = Math.floor(Math.random() * (canvas.width - 300 + 1)) + 300
+        this.y = Math.floor(Math.random() * (canvas.height - 300 + 1)) + 300
 
-        this.w = 40
-        this.h = 45
+        this.w = 32
+        this.h = 32
+
+        this.middlePos = {x: this.x + (this.w / 2), y: this.y + (this.h / 2)}
+
+        this.image = new Image()
+        this.image.src = './models/enemy.png'
 
         // so speed is never 0
         this.xSpeed = 0
         while (this.xSpeed === 0) {
-            this.xSpeed = Math.floor(Math.random() * (6 - 4 + 1)) + 4
+            this.xSpeed = Math.random() * (6 - 4 + 1) + 4
         }
 
         this.ySpeed = 0
@@ -294,64 +305,108 @@ class Enemy {
             this.ySpeed = Math.floor(Math.random() * (6 - 4 + 1)) + 4
         }
 
+        // fire rate in milisecond
+        this.fireRate = 1000
+
+        // damage per hit
+        this.dph = 2
+
+        // damage per seconds
+        this.dps = (this.fireRate / 1000) * this.dph
+
+        this.targetPos = null
         this.targetPlayer = this.#setTarget()
     }
 
     #setTarget(){
         let player1Pos = {x: player1.x + player1.w / 2, y: player1.y + player1.h / 2}
         let player2Pos = {x: player2.x + player2.w / 2, y: player2.y + player2.h / 2}
-        let thisPos = {x: this.x + this.w / 2, y: this.y + this.h / 2}
 
-        let d1 = (player1Pos.x - thisPos.x) ** 2 + (player1Pos.y - thisPos.y) ** 2
-        let d2 = (player2Pos.x - thisPos.x) ** 2 + (player2Pos.y - thisPos.y) ** 2
+        let d1 = (player1Pos.x - this.middlePos.x) ** 2 + (player1Pos.y - this.middlePos.y) ** 2
+        let d2 = (player2Pos.x - this.middlePos.x) ** 2 + (player2Pos.y - this.middlePos.y) ** 2
 
-        return d1 < d2 ? player1 : player2
+        if (d1 < d2) {
+            this.targetPos = player1Pos
+            return player1
+        } else if (d1 > d2){
+            this.targetPos = player2Pos
+            return player2
+        } else {
+            if (Math.floor(Math.random()) === 0) {
+                this.targetPos = player1Pos
+                return player1
+            } else {
+                this.targetPos = player2Pos
+                return player2
+            }
+        }
+    }
+
+    #updatePos(){
+        this.middlePos = {x: this.x + (this.w / 2), 
+                        y: this.y + (this.h / 2)}
+
+        this.targetPlayer = this.#setTarget()
+    }
+
+    #hitDetection(){
+        if (this.targetPlayer.x < this.x + this.w &&
+            this.targetPlayer.x + this.targetPlayer.w > this.x && 
+            this.targetPlayer.y < this.y + this.h && 
+            this.targetPlayer.y + this.targetPlayer.h > this.y) 
+        {
+            return true
+        }
+        return false
     }
 
     update(){
+        this.#updatePos()
+        this.#hitDetection() ? console.log("yes") : null
 
-        this.targetPlayer = this.#setTarget()
         // Move towards target player
-        let distanceToTarget = Math.sqrt((this.x - this.targetPlayer.x) ** 2 + (this.y - this.targetPlayer.y) ** 2)
+        let distanceToTarget = Math.sqrt((this.middlePos.x - this.targetPos.x) ** 2 + (this.middlePos.y - this.targetPos.y) ** 2);
 
-        const dx = (this.targetPlayer.x - this.x) / distanceToTarget * this.xSpeed
-        const dy = (this.targetPlayer.y - this.y) / distanceToTarget * this.ySpeed;
-        this.x += dx;
-        this.y += dy;
+        const dx = (this.targetPos.x - this.middlePos.x) / distanceToTarget * this.xSpeed;
+        const dy = (this.targetPos.y - this.middlePos.y) / distanceToTarget * this.ySpeed;
 
-        // boundary checks
-        if (this.x <= 0) {
-            console.table(this.x, this.y)
-
-            this.x = 0
+        // Check if new position is inside the boundary
+        if (this.x + dx >= 0 && this.x + dx + this.w <= canvas.width &&
+            this.y + dy >= 0 && this.y + dy + this.h <= canvas.height) {
+            this.x += dx;
+            this.y += dy;
         }
+        else {
+            // If new position is outside the boundary, move the enemy to the nearest valid position
+            if (this.x + dx < 0) {
+                this.x = 0
+            }
 
-        if (this.x + this.w >= CANVAS_WIDTH) {
-            console.table(this.x, this.y)
+            else if (this.x + dx + this.w > canvas.width) {
+                this.x = canvas.width - this.w;
+            }
 
-            this.x = CANVAS_WIDTH - this.w
+            if (this.y + dy < 0) { 
+                this.y = 0;
+            }
+            else if (this.y + dy + this.h > canvas.height) {
+                this.y = canvas.height - this.h;
+            }
         }
-
-        if (this.y <= 0) {
-            console.table(this.x, this.y)
-
-            this.y = 0
-        }
-
-        if (this.y + this.h >= CANVAS_HEIGHT) {
-            console.table(this.x, this.y)
-            this.y = CANVAS_HEIGHT - this.h
-        }
-    }
+}
 
     draw(){
         this.update()
 
+        // HITBOX
+
         ctx.beginPath()
-        ctx.fillStyle = 'red'
+        ctx.strokeStyle = 'red'
         ctx.rect(this.x, this.y, this.w, this.h)
-        ctx.fill()
+        ctx.stroke()
         ctx.closePath()
+
+        ctx.drawImage(this.image, this.x, this.y, this.w, this.h)
     }
 }
 
@@ -367,7 +422,7 @@ for (let i = 0; i < numberOfEnemies; i++) {
 
 function animate() {
 
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     player1.draw('blue')
     player2.draw('green')
